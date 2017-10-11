7c98c06cf4bc080eb800262b229c1b0d0f5a8171
q.w <chu8129@users.noreply.github.com>
2017-10-11 19:24:03 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -3,3 +3,16 @@ chmod +x shadowsocks.sh
 ./shadowsocks.sh 2>&1 | tee shadowsocks.log
 https://twitter.com/clowwindy
 https://teddysun.com/342.html
+
+
+{
+    "server":"0.0.0.0",
+    "server_port":8129,
+    "local_address":"127.0.0.1",
+    "local_port":1080,
+    "password":"",
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open":true,
+    "workers": 1
+}
