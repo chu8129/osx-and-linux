20c5043f57be9568397e9c84fc67b65ddd90b6e1
q.w <chu8129@users.noreply.github.com>
2019-03-28 10:36:43 +0800

    Update kcp.start.sh

--- kcp.start.sh
+++ kcp.start.sh
@@ -7,10 +7,27 @@ git clone https://github.com/chu8129/marks
 cp marks/kcp.proxy.py ./proxy.py
 rm -rf marks
 touch start.sh
-echo "ps -ef|grep proxy.py|awk '{print $2}'|xargs kill -9" >> start.sh
-echo "nohup python proxy.py 8132 1>/dev/null 2>&1 &" >> start.sh
-echo "ps -ef|grep server_linux_amd64|awk '{print $2}'|xargs kill -9" >> start.sh
-echo "nohup ./server_linux_amd64 -t localhost:8132 -l :8131 -mode fast3 -mtu=512 1>/dev/null 2>&1 &" >> start.sh
-echo "ps -ef|grep server_linux_amd64" >> start.sh
+
 chmod +x start.sh
 echo "*/5 * * * * /kcp/start.sh" >> /var/spool/cron/root
+
+
+
+#!/bin/bash
+while true
+do
+    kill1="ps -ef|grep proxy.py|awk '{print \$2}'|xargs kill -9"
+    ps1="ps -ef|grep proxy"
+    no1="nohup python proxy.py 8132 1>/dev/null 2>&1 &"
+    kill2="ps -ef|grep server_linux_amd64|awk '{print \$2}'|xargs kill -9"
+    no2="nohup ./server_linux_amd64 -t localhost:8132 -l :8131 -mode fast3 -mtu=512 1>/dev/null 2>&1 &"
+    ps2="ps -ef|grep server_linux_amd64"
+    eval $kill1
+    eval $ps1
+    eval $no1
+    eval $kill2
+    eval $no2
+    eval $ps2
+    s="sleep 600"
+    eval $s
+done
